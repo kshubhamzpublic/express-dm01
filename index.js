bc542357281316f5c1ace70487f6b48349c163eb
Shubham Kumar <shubhamkumar@Shubhams-MacBook-Air.local>
2022-07-12 14:00:46 +0530

    updated with mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-const express = require("express");
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+
+const productSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(
+      "MongoDB connection successful! Registering get resources endpoint.."
+    );
+
+    app.get("/resources", async (req, res) => {
+      const allProducts = await Product.find({});
+      res.send(allProducts);
+    });
+
+    console.log("endpoint registered!!");
+  })
+  .catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send({
